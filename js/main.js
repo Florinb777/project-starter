18f8666dae2dd448f647c537202a79d198872682
Florin Barbu <florinb77@yahoo.com>
2023-03-07 20:39:20 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
-// Just a starter file in case you need it
+// Just <script src="https://cdn.logwork.com/widget/countdown.js"></script>
+<a href="https://logwork.com/countdown-yyws" class="countdown-timer" data-timezone="Europe/Bucharest" data-language="ro" data-date="2023-03-09 19:34">Countdown Timer</a>a starter file in case you need it
